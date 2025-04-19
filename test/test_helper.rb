8aecf5e0d1ec92d180636b431344252792933c3f
Andrew Kane <andrew@ankane.org>
2025-04-19 12:45:43 -0700

    Improved tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,13 +12,18 @@ ActiveRecord::Migration.verbose = ENV["VERBOSE"]
 adapter = ENV["ADAPTER"] || "postgresql"
 ActiveRecord::Base.establish_connection adapter: adapter, database: "hypershield_test"
 
-ActiveRecord::Base.connection.execute("DROP VIEW IF EXISTS hypershield.users")
-ActiveRecord::Base.connection.execute("DROP SCHEMA IF EXISTS hypershield")
+if adapter == "postgresql"
+  ActiveRecord::Base.connection.execute("DROP SCHEMA IF EXISTS hypershield CASCADE")
+else
+  ActiveRecord::Base.connection.execute("DROP SCHEMA IF EXISTS hypershield")
+end
 ActiveRecord::Base.connection.execute("CREATE SCHEMA hypershield")
 
-ActiveRecord::Migration.create_table :users, force: true do |t|
-  t.string :name
-  t.string :encrypted_email
+ActiveRecord::Schema.define do
+  create_table :users, force: true do |t|
+    t.string :name
+    t.string :encrypted_email
+  end
 end
 
 Hypershield.log_sql = true
